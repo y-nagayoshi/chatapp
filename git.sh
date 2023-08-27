1c36d216829f7bb5d649bc30e20975a9f05f33c9
yusuke <yusuke@yusukenoMacBook-Pro.local>
2023-08-27 22:00:09 +0900

    commit: test

--- git.sh
+++ git.sh
@@ -3,8 +3,10 @@ echo "enter your commit comment"
 read comment # ここで読み込み待ち
 echo ${comment}
 
+commit_message="commit: $comment"
+
 git init
 git status
 git add -A
-git commit -m '$comment'
+git commit -m "$commit_message"
 git push -u origin main
