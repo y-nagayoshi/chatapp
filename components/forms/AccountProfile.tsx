cac0ce9f0d4c8a39d1f700e3682b5f49c55656e9
yusuke <yusuke@yusukenoMacBook-Pro.local>
2023-08-27 16:36:36 +0900

    commit

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import React from 'react'
+import React, { ChangeEvent } from 'react'
+import Image from 'next/image';
 import {
     Form,
     FormControl,
@@ -42,30 +43,120 @@ const AccountProfile = ({user, btnTitle}:Props) => {
         },
       });
 
+    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+      e.preventDefault();
+    }
+
     function onSubmit(values: z.infer<typeof UserValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    // console.log(values)
     }
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <form 
+      onSubmit={form.handleSubmit(onSubmit)} 
+      className="flex flex-col justify-start gap-10"
+    >
+      <FormField
+        control={form.control}
+        name="profile_photo"
+        render={({ field }) => (
+          <FormItem className='flex items-center gap-4'>
+            {/* <FormLabel className='account-form_image-label'> */}
+            <FormLabel>
+              {
+                field.value ? (
+                  <Image 
+                    src={field.value}
+                    alt='profile photo'
+                    width={96}
+                    height={96}
+                    priority
+                    className='rounded-full object-contain'
+                  />
+                ) : (
+                  <Image 
+                    src='/assets/profile.svg'
+                    alt='profile photo'
+                    width={24}
+                    height={24}
+                    priority
+                    className='object-contain'
+                  />
+                )
+              }
+            </FormLabel>
+            <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <Input 
+                placeholder='upload a photo'
+                accept='image/*'
+                className='account-form_image-input'
+                onChange={(e) => handleImage(e, field.onChange)}  
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem className='flex items-center gap-3 w-full'>
+            <FormLabel className='text-base-semibold text-light-2'>
+              Name
+            </FormLabel>
+            <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <Input 
+                type='text'
+                className='account-form_input no-focus'
+                onChange={(e) => handleImage(e, field.onChange)}  
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
       <FormField
         control={form.control}
         name="username"
         render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
-            <FormControl>
-              <Input placeholder="shadcn" {...field} />
+          <FormItem className='flex items-center gap-3 w-full'>
+            <FormLabel className='text-base-semibold text-light-2'>
+              Username
+            </FormLabel>
+            <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <Input 
+                type='text'
+                className='account-form_input no-focus'
+                onChange={(e) => handleImage(e, field.onChange)}  
+              />
             </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
           </FormItem>
         )}
       />
+
+      <FormField
+        control={form.control}
+        name="bio"
+        render={({ field }) => (
+          <FormItem className='flex items-center gap-3 w-full'>
+            <FormLabel className='text-base-semibold text-light-2'>
+              Bio
+            </FormLabel>
+            <FormControl className='flex-1 text-base-semibold text-gray-200'>
+              <Textarea
+                rows={10}
+                className='account-form_input no-focus'
+                onChange={(e) => handleImage(e, field.onChange)}  
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+
       <Button type="submit">Submit</Button>
     </form>
   </Form>
--- next.config.js
+++ next.config.js
@@ -2,3 +2,16 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
