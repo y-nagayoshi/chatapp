94fc24c624956e7150aeac8421a615b1a4dc7388
yusuke <yusuke@yusukenoMacBook-Pro.local>
2023-08-28 22:35:34 +0900

    modified PostThread

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostThread = ({userId}:{userId:string}) => {
+  return (
+    <div>PostThread</div>
+  )
+}
+
+export default PostThread
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import User from '../models/user.model'
 import { connectToDB } from '../mongoose';
+import { AnySoaRecord } from 'dns';
 
 interface Params {
     userId: string,
@@ -44,4 +45,13 @@ export async function updateUser({
         throw new Error(`Failed to create/update user: ${error.message}`)
     }
 
+}
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+        return await User.findOne({id: userId})
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`)
+    }
 }
