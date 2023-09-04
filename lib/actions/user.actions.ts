5106febac09317d66d953fb4eedacebef4e633d6
yusuke <wakasagi135@gmail.com>
2023-09-04 22:13:02 +0900

    modified search page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { revalidatePath } from 'next/cache';
 import User from '../models/user.model'
 import { connectToDB } from '../mongoose';
 import { AnySoaRecord } from 'dns';
+import Thread from '../models/thread.model';
+import { SortOrder, FilterQuery } from 'mongoose';
 
 interface Params {
     userId: string,
@@ -62,9 +64,78 @@ export async function fetchUserPosts(userId:string){
 
         const threads = await User.findOne({id: userId})
         .populate({
-            path: 'children'
+            path: 'threads',
+            model: Thread,
+            populate: {
+                path: 'children',
+                model: Thread,
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: 'name image id'
+                }
+            }
         })
-    } catch (error) {
-
+        return threads
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user posts: ${error.message}`)
     }
 }
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc",
+  }: {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+  }) {
+    try {
+      connectToDB();
+  
+      // Calculate the number of users to skip based on the page number and page size.
+      const skipAmount = (pageNumber - 1) * pageSize;
+  
+      // Create a case-insensitive regular expression for the provided search string.
+      const regex = new RegExp(searchString, "i");
+  
+      // Create an initial query object to filter users.
+      const query: FilterQuery<typeof User> = {
+        id: { $ne: userId }, // Exclude the current user from the results.
+      };
+  
+      // If the search string is not empty, add the $or operator to match either username or name fields.
+      if (searchString.trim() !== "") {
+        query.$or = [
+          { username: { $regex: regex } },
+          { name: { $regex: regex } },
+        ];
+      }
+  
+      // Define the sort options for the fetched users based on createdAt field and provided sort order.
+      const sortOptions = { createdAt: sortBy };
+  
+      const usersQuery = User.find(query)
+        .sort(sortOptions)
+        .skip(skipAmount)
+        .limit(pageSize);
+  
+      // Count the total number of users that match the search criteria (without pagination).
+      const totalUsersCount = await User.countDocuments(query);
+  
+      const users = await usersQuery.exec();
+  
+      // Check if there are more users beyond the current page.
+      const isNext = totalUsersCount > skipAmount + users.length;
+  
+      return { users, isNext };
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      throw error;
+    }
+  }
