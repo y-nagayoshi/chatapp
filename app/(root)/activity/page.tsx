e7578819dd1d47447f6d5cb901f24421d4fce446
yusuke <wakasagi135@gmail.com>
2023-09-05 06:12:13 +0900

    added activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,7 +1,43 @@
-const Page = async () => {
+import { fetchUser, fetchUsers, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+async function Page() {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    const activity = await getActivity(userInfo._id);
+    
     return (
         <section>
-            <h1 className="head-text mb-10">search</h1>
+            <h1 className="head-text mb-10">Activity</h1>
+            <section className="mt-10 flex flex-col gap-5">
+            {activity?.length > 0 ? (
+                <>
+                {activity?.map((activity) => (
+                    <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                        <article className="activity-card">
+                            <Image 
+                                src={activity.author.image}
+                                alt='Profile Picture'
+                                width={20}
+                                height={20}
+                                className="rounded-full object-cover"
+                            />
+                        </article>
+                    </Link>
+                ))}
+                </>        
+            ) : <p>No activity yet</p>
+            }
+            </section>
         </section>
     )
 } 
